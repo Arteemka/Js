9ee98dba0c271c1ef8ba8b2b69f07d7f584dc2f8
Artem <artemiy.rak@mail.ru>
2018-11-16 20:34:24 +0300

    Lesson

--- Lesson1/basic_lesson/js/script.js
+++ Lesson1/basic_lesson/js/script.js
@@ -1,28 +1,26 @@
 function main() {
         'use strict';
 
-        let i = 0,a,b,c,d,income = [];
+        let i = 0,a,b,c,d;
         let money = prompt("Ваш бюджет на месяц?", "0");
         let time = prompt("Введите дату в формате YYYY-MM-DD", "0");
+     
+        let appData = {
+        money,
+        timeData: time,
+        expenses: {},
+        income: [],
+        optionalExpenses: null,
+        savings: false
+        };
 
         a = prompt("Введите обязательную статью расходов в этом месяце", "0");
         b = prompt("Во сколько обойдется?", "0");
         c = prompt("Введите обязательную статью расходов в этом месяце", "0");
         d = prompt("Во сколько обойдется?", "0");
-                
-
-        let appData = {
-        money,
-        timeData: time,
-        expenses: {},
-        income: income,
-        optionalExpenses: null,
-        savings: false
-        };
 
         appData.expenses[a] = b;
         appData.expenses[c] = d;
-
 
         console.log(appData.expenses);
         alert(appData.money/30);
---
+++ Lesson2/basic_lesson/js/script.js
@@ -0,0 +1,61 @@
+function main() {
+        'use strict';
+
+        let money = prompt("Ваш бюджет на месяц?", "0");
+        let time = prompt("Введите дату в формате YYYY-MM-DD", "0");
+     
+        let appData = {
+        money,
+        timeData: time,
+        expenses: {},
+        income: [],
+        optionalExpenses: null,
+        savings: false
+        };
+
+        // for(let i = 0; i < 2; i++){
+        //         let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        //             b = prompt("Во сколько обойдется?", "");
+
+        //         if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+        //                 && a != '' && b != '' && a.length < 50){
+        //                 appData.expenses[a] = b;
+        //         }else {
+        //                 console.log('Error!');
+        //                 i--;
+        //         }
+        // }
+
+        // let i = 0;
+        // while(i < 2){
+        //         let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        //             b = prompt("Во сколько обойдется?", "");
+
+        //         if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+        //                 && a != '' && b != '' && a.length < 50){
+        //                 appData.expenses[a] = b;
+        //                 i++;
+        //         }else {
+        //                 console.log('Error!');                                  
+        //         }
+        // }
+
+        let i = 0;
+        do{
+                let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+                    b = prompt("Во сколько обойдется?", "");
+
+                if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50){
+                        appData.expenses[a] = b;
+                        i++;
+                }else {
+                        console.log('Error!'); 
+                                                       
+                }
+        }while(i < 2);
+
+        alert(appData.money/30);
+
+}
+
+main();
