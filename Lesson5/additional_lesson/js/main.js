b633bd05bae98df5b94b7a790df7791aae2ecdb5
Artem <artemiy.rak@mail.ru>
2018-11-25 01:49:31 +0300

    commit

--- Lesson5/additional_lesson/js/main.js
+++ Lesson5/additional_lesson/js/main.js
@@ -9,44 +9,75 @@ function main() {
         Minutes,
         Seconds;
 
-        Day = Data.getDay();
-        Month = Data.getMonth()+1;
-        Year = Data.getFullYear();
-        Hours = Data.getHours()+1;
-        Minutes = Data.getMinutes();
-        Seconds = Data.getSeconds();
+    Day = Data.getDate();
+    Month = Data.getMonth() + 1;
+    Year = Data.getFullYear();
+    Hours = Data.getHours() + 1;
+    Minutes = Data.getMinutes();
+    Seconds = Data.getSeconds();
 
-        if (Day < 10) {
-            Day = '0' + Day;}
+    if (Day < 10) {
+        Day = '0' + Day;
+    }
 
-        if (Month < 10){ Month = '0' + Month;}
+    if (Month < 10) {
+        Month = '0' + Month;
+    }
 
-        
-        let p = document.createElement("p");
-        p.id = "pan";
-        document.body.appendChild(p); 
-        let date = (Hours + ":" + Minutes + ":" + Seconds + " " + Day + ":" + Month + ":" + Year);
-                      p.insertAdjacentHTML('beforeBegin', date);
 
-                      let week = ["Monday", "Tuesday", 
-                      "Wednesday", "Thursday",
-                       "Friday", "Saturday", "Sunday"], 
-                      i = 0;
-      
-          let div = document.createElement("div");
-              div.id = "week";
-              document.body.appendChild(div);
-          let Week = document.getElementById("week"); 
-      
-          while (i < week.length) {
-               if (i == Day - 1) {
-                      let strong =  week[i] + "<br>";
-                      Week.insertAdjacentHTML('beforeBegin', strong);
-                      
-                   }
-                   i++;
-          }
-      
+    let p = document.createElement("p");
+    p.id = "pan";
+    document.body.appendChild(p);
+    let date = (Hours + ":" + Minutes + ":" + Seconds + " " + Day + ":" + Month + ":" + Year);
+    p.insertAdjacentHTML('beforeBegin', date);
 
+
+    let div = document.createElement("div");
+    div.id = "week";
+    document.body.appendChild(div);
+    let Week = document.getElementById("week");
+
+    function getWeekDay(date) {
+        date = date || new Date();
+        var days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+        var day = date.getDay();
+        return days[day];
+    }
+
+    Week.insertAdjacentHTML('beforeBegin', getWeekDay(Data));
+
+
+    let input1 = document.createElement('input');
+    input1.id = "input1";
+    document.body.appendChild(input1);
+    let Input1 = document.getElementById("input1");
+
+    let input2 = document.createElement('input');
+    input2.id = "input2";
+    document.body.appendChild(input2);
+    let Input2 = document.getElementById("input2");
+
+
+
+    let button = document.createElement('button');
+    button.id = "button";
+    document.body.appendChild(button);
+    document.getElementById("button").innerText = 'OK';
+    let Button = document.getElementById("button");
+    Button.addEventListener('click', function () {
+
+        a(Input1.value, Input2.value);
+
+        function a(m, b) {
+
+            let firstarray = m.match(/(\d{2})\.(\d{2})\.(\d{4})/);
+            let secondarray = b.match(/(\d{2})\.(\d{2})\.(\d{4})/);
+            let first = Date.UTC(firstarray[3], firstarray[2] - 1, firstarray[1]);
+            let second = Date.UTC(secondarray[3], secondarray[2] - 1, secondarray[1]);
+            let days = Math.ceil((second - first) / (1000 * 60 * 60 * 24));
+            alert(days);
+        }
+    });
 }
+
 main();
--- Lesson6/main_basic/js/main.js
+++ Lesson6/main_basic/js/main.js
@@ -22,23 +22,22 @@ function main() {
         yearValue = document.querySelector('.year-value'),
         monthValue = document.querySelector('.month-value'),
         dayValue = document.querySelector('.day-value');
-        
 
-        
-        expensesBtn.disabled = true;
-        optionalExpensesBtn.disabled = true;
-        countBudgetBtn.disabled = true;
-        expensesBtn.style.backgroundImage = "linear-gradient(336deg,#ccc,#ccc)";
-        optionalExpensesBtn.style.backgroundImage = "linear-gradient(336deg,#ccc,#ccc)";
-        countBudgetBtn.style.backgroundImage = "linear-gradient(336deg,#ccc,#ccc)";
+
+
+    expensesBtn.disabled = true;
+    optionalExpensesBtn.disabled = true;
+    countBudgetBtn.disabled = true;
+    expensesBtn.style.backgroundImage = "linear-gradient(336deg,#ccc,#ccc)";
+    optionalExpensesBtn.style.backgroundImage = "linear-gradient(336deg,#ccc,#ccc)";
+    countBudgetBtn.style.backgroundImage = "linear-gradient(336deg,#ccc,#ccc)";
 
     let money, time, sum = 0;
 
     startButton.addEventListener('click', function () {
-      
+
         time = prompt("Введите дату в формате YYYY-MM-DD", "0");
         money = prompt("Ваш бюджет на месяц?", "0");
-
 
         while (isNaN(money) || money === "" || money === null) {
             money = prompt("Ваш бюджет на месяц?", "0");
@@ -49,8 +48,8 @@ function main() {
         budgetValue.textContent = money;
         yearValue.value = new Date(Date.parse(time)).getFullYear();
         monthValue.value = new Date(Date.parse(time)).getMonth() + 1;
-        dayValue.value = new Date(Date.parse(time)).getDay(); 
-        
+        dayValue.value = new Date(Date.parse(time)).getDate();
+
         expensesBtn.disabled = false;
         optionalExpensesBtn.disabled = false;
         countBudgetBtn.disabled = false;
@@ -58,141 +57,130 @@ function main() {
         expensesBtn.style.backgroundImage = "linear-gradient(336deg,#ffbd75,#ff964b)";
         optionalExpensesBtn.style.backgroundImage = "linear-gradient(336deg,#ffbd75,#ff964b)";
         countBudgetBtn.style.backgroundImage = "linear-gradient(336deg,#ffbd75,#ff964b)";
-    
+
     });
 
     expensesBtn.addEventListener('click', function () {
-        let check =false;
-        expensesItem.forEach(function(item){
-            if(item.value != '' && item.value != null){
-                 check  = true; 
-                 item.style.border = "1px solid #fff";                 
-                } else {
-                    check = false;
-                    item.style.border = "1px solid red";
-                }
-        });
-        
-            
-            console.log(check);
-            if(check){
-           
-                for (let i = 0; i < expensesItem.length; i++) {
-                   let a = expensesItem[i].value,
-                    b = expensesItem[++i].value;
-                                
-                    if ((typeof (a)) === 'string' && a != null &&
-                        b != null && a != '' && b != '' && a.length < 50) {
-                        appData.expenses[a] = b;
-                        sum += +b;
-                    } else {
-                       
-                      return alert('Ошибка');
-                    
-                   
-                    }
-                }
-                expensesValue.textContent = sum.toFixed();
-                
-            }
-        
-        
 
+        let check = false;
+        expensesItem.forEach(function (item) {
+
+            if (item.value != '' && item.value != null) {
+                check = true;
+                item.style.border = "1px solid #fff";
+            } else {
+                check = false;
+                item.style.border = "1px solid red";
+            }
+        });
+
+        if (check) {
+            for (let i = 0; i < expensesItem.length; i++) {
+                let a = expensesItem[i].value,
+                    b = expensesItem[++i].value;
+
+                if ((typeof (a)) === 'string' && a != null &&
+                    b != null && a != '' && b != '' && a.length < 50) {
+                    appData.expenses[a] = b;
+                    sum += +b;
+                } else {
+                    return alert('Ошибка');
+                }
+            }
+            expensesValue.textContent = sum.toFixed();
+        }
     });
 
-    
+
 
     optionalExpensesBtn.addEventListener('click', function () {
-        let check =false;
 
-        expensesItem.forEach(function(item){
-            if( item.value != '' && item.value != null) {
-                 check  = true; 
-                 item.style.border = "1px solid #fff";                 
-                } else {
-                    check = false;
-                    item.style.border = "1px solid red";
-                }
-        });
-            console.log(check);
+        let check = false;
+        expensesItem.forEach(function (item) {
 
-            if(check){
-             let string = '';
-        for (let i = 0; i < 3; i++) {
-            
-            let opt = optionalExpensesItem[i].value;        
-                    appData.optionalExpenses[i] = opt;
-                    string += opt + ' '; 
-            }
-            optionalexpensesValue.textContent = string ;
-        }
-            
-            
-    });
-   
-    countBudgetBtn.addEventListener('click', function () {
-        
-        let check =false;
-        expensesItem.forEach(function(item){
-            if(item.value != '' && item.value != null) {
-                 check  = true; 
-                 item.style.border = "1px solid #fff";                 
-                } else {
-                    check = false;
-                    item.style.border = "1px solid red";
-                }
-        });
-            console.log(check);
-            if(check){
-           
-        
-        if (appData.budget != undefined) {
-            appData.moneyPerDay = ((appData.budget - sum)/ 30).toFixed();
-            
-    
-            if (appData.moneyPerDay > 0 && appData.moneyPerDay < 100) {
-                levelValue.textContent = "Минимальный уровень достатка";
-                dayBudgetValue.textContent = appData.moneyPerDay;
-            } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-                levelValue.textContent = "Средний уровень достатка";
-                dayBudgetValue.textContent = appData.moneyPerDay;
-            } else if (appData.moneyPerDay > 2000) {
-                levelValue.textContent = "Высокий уровень достатка";
-                dayBudgetValue.textContent = appData.moneyPerDay;
+            if (item.value != '' && item.value != null) {
+                check = true;
+                item.style.border = "1px solid #fff";
             } else {
-                levelValue.textContent = "Произошла ошибка!";
+                check = false;
+                item.style.border = "1px solid red";
             }
-        } else {
-            dayBudgetValue.textContent = 'Произошла ошибка';}
+        });
+
+        if (check) {
+            let string = '';
+            for (let i = 0; i < 3; i++) {
+                let opt = optionalExpensesItem[i].value;
+                appData.optionalExpenses[i] = opt;
+                string += opt + ' ';
+            }
+            optionalexpensesValue.textContent = string;
         }
-       
+    });
+
+    countBudgetBtn.addEventListener('click', function () {
+
+        let check = false;
+        expensesItem.forEach(function (item) {
+            if (item.value != '' && item.value != null) {
+                check = true;
+                item.style.border = "1px solid #fff";
+            } else {
+                check = false;
+                item.style.border = "1px solid red";
+            }
+        });
+
+        if (check) {
+            if (appData.budget != undefined) {
+                appData.moneyPerDay = ((appData.budget - sum) / 30).toFixed();
+                if (appData.moneyPerDay > 0 && appData.moneyPerDay < 100) {
+                    levelValue.textContent = "Минимальный уровень достатка";
+                    dayBudgetValue.textContent = appData.moneyPerDay;
+                } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+                    levelValue.textContent = "Средний уровень достатка";
+                    dayBudgetValue.textContent = appData.moneyPerDay;
+                } else if (appData.moneyPerDay > 2000) {
+                    levelValue.textContent = "Высокий уровень достатка";
+                    dayBudgetValue.textContent = appData.moneyPerDay;
+                } else {
+                    levelValue.textContent = "Произошла ошибка!";
+                }
+            } else {
+                dayBudgetValue.textContent = 'Произошла ошибка';
+            }
+        }
+
     });
 
     chooseIncome.addEventListener('input', function () {
 
-      let items = chooseIncome.value;
-          appData.income = items.split(", ");
-          incomeValue.textContent = appData.income;
+        let items = chooseIncome.value;
+        appData.income = items.split(", ");
+        incomeValue.textContent = appData.income;
     });
 
-    chooseSum.style.backgroundColor = "#ccc";   
-        choosePercent.style.backgroundColor = "#ccc"; 
-        chooseSum.setAttribute("readonly", true);
-        choosePercent.setAttribute("readonly", true);
+    chooseSum.style.backgroundColor = "#ccc";
+    choosePercent.style.backgroundColor = "#ccc";
+    chooseSum.setAttribute("readonly", true);
+    choosePercent.setAttribute("readonly", true);
 
     checkSavings.addEventListener('input', function () {
+
         checkSavings = false;
 
-         if (appData.savings == true) {
+        if (appData.savings == true) {
             appData.savings = false;
-            chooseSum.style.backgroundColor = "#ccc";   
-        choosePercent.style.backgroundColor = "#ccc"; 
-        chooseSum.setAttribute("readonly", true);
-        choosePercent.setAttribute("readonly", true);
+            chooseSum.style.backgroundColor = "#ccc";
+            choosePercent.style.backgroundColor = "#ccc";
+            chooseSum.setAttribute("readonly", true);
+            choosePercent.setAttribute("readonly", true);
+            chooseSum.value = '';
+            choosePercent.value = '';
         } else {
             appData.savings = true;
-            chooseSum.style.backgroundColor = "#fff";   
-            choosePercent.style.backgroundColor = "#fff"; 
+            chooseSum.style.backgroundColor = "#fff";
+            choosePercent.style.backgroundColor = "#fff";
             chooseSum.removeAttribute("readonly");
             choosePercent.removeAttribute("readonly");
         }
@@ -204,11 +192,11 @@ function main() {
             let sum = +chooseSum.value,
                 percent = +choosePercent.value;
 
-                appData.monthInCome = sum / 100 / 12 * percent;   
-                appData.yearInCome = sum / 100 * percent; 
+            appData.monthInCome = sum / 100 / 12 * percent;
+            appData.yearInCome = sum / 100 * percent;
 
-                monthsavingsValue.textContent = appData.monthInCome.toFixed(1);
-                yearsavingsValue.textContent = appData.yearInCome.toFixed(1);
+            monthsavingsValue.textContent = appData.monthInCome.toFixed(1);
+            yearsavingsValue.textContent = appData.yearInCome.toFixed(1);
         }
     });
 
@@ -218,11 +206,11 @@ function main() {
             let sum = +chooseSum.value,
                 percent = +choosePercent.value;
 
-                appData.monthInCome = sum / 100 / 12 * percent;   
-                appData.yearInCome = sum / 100 * percent; 
+            appData.monthInCome = sum / 100 / 12 * percent;
+            appData.yearInCome = sum / 100 * percent;
 
-                monthsavingsValue.textContent = appData.monthInCome.toFixed(1);
-                yearsavingsValue.textContent = appData.yearInCome.toFixed(1);
+            monthsavingsValue.textContent = appData.monthInCome.toFixed(1);
+            yearsavingsValue.textContent = appData.yearInCome.toFixed(1);
         }
     });
 
@@ -233,7 +221,7 @@ function main() {
         income: [],
         optionalExpenses: {},
         savings: false,
-        
+
     };
 
     for (let key in appData) {
